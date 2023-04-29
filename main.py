178559ab0af8e71195050ee03f75e5c5b64ada14
Dako <sashadakopro@gmail.com>
2023-04-29 10:02:42 +0300

    add rectangle

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ import cv2
 image = cv2.imread("Baiden.jpg")
 
 
+casc = cv2.CascadeClassifier("haarcascade_eye.xml")
+eyes = casc.detectMultiScale(image)
+
+print(eyes)
+
+for (x,y,w,h) in eyes:
+    cv2.rectangle(image, (x,y), (x+w, y+h), (0,0,255), 3)
+
+
 
 cv2.imshow("Baiden", image)
 cv2.waitKey()
-print (image)
+
